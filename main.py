9fa569ebb3f011f759d36fe63a37524a869fdbd2
marc1096 <marc1096@hotmail.com>
2025-10-13 17:44:57 -0400

    updated stats and main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+from stats import count_words
+def get_book_text(frankenstein):
+    with open(frankenstein) as f:
+        return f.read()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    result = count_words(text)
+    print(f"Found {result} total words")
+
+main()
+
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(text):
+    word_count = text.split()
+    number_of_words = len(word_count)
+    return number_of_words
