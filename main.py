f4aae348c3957f4756d6c2ab62b81aa9101a02bc
marc1096 <marc1096@hotmail.com>
2025-10-14 18:45:53 -0400

    added count_chars and updated stats.py and main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from stats import count_words
+from stats import count_chars
 def get_book_text(frankenstein):
     with open(frankenstein) as f:
         return f.read()
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    result = count_words(text)
-    print(f"Found {result} total words")
+    word_count_result = count_words(text)
+    char_counts_result=count_chars(text)
+    print(f"Found {word_count_result} total words")
+    print(char_counts_result)
 
 main()
+
 
--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def count_words(text):
     word_count = text.split()
     number_of_words = len(word_count)
     return number_of_words
+
+def count_chars(text):
+    each_character_appears = {}
+    for character in text:
+        lowercased_character = character.lower()
+        if lowercased_character in each_character_appears:
+            each_character_appears[lowercased_character] += 1
+        else: each_character_appears[lowercased_character] = 1
+    return each_character_appears
