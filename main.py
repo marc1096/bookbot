6a98fe70c0a7f66e1cb0c24af2227f9a88b45e6e
marc1096 <marc1096@hotmail.com>
2025-10-23 19:58:33 -0400

    added mobydick.txt and prideandprejudice.txt files and text also added sys.argv

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+import sys
 from stats import count_words, count_chars, sorted_list
-def get_book_text(frankenstein):
-    with open(frankenstein) as f:
+
+def get_book_text(path):
+    with open(path) as f:
         return f.read()
+
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
-
-
     word_count_result = count_words(text)
-    char_counts_result=count_chars(text)
+    char_counts_result = count_chars(text)
+
+
+
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count_result} total words")
     print("--------- Character Count -------")
